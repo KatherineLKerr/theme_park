e5133d1f56606b9a97b4bcf3748c13628b041524
Katherine Kerr <katherinekerr@protonmail.com>
2019-07-30 19:12:58 +0100

    dodgems implements ITicketed

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,18 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
     }
-
 
     @Test
     public void hasName() {
@@ -30,4 +32,15 @@ public class DodgemTest {
         assertEquals(0, dodgems.getVisitCount());
     }
 
+    @Test
+    public void canChargeForTicket() {
+        visitor1 = new Visitor(98, 180, 23);
+        assertEquals(4.50, dodgems.priceFor(visitor1), 0.01);
+    }
+
+    @Test
+    public void canChargeHalfForUnder12() {
+        visitor2 = new Visitor(3, 180, 23);
+        assertEquals(2.25, dodgems.priceFor(visitor2), 0.01);
+    }
 }
