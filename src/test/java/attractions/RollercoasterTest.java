8a3b6e0f774ecdb7dc9afd7a1040f408e044311e
Katherine Kerr <katherinekerr@protonmail.com>
2019-07-30 18:07:26 +0100

    adds tests for implementing ISecurity in rollercoasterTest

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,12 +2,17 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
+    Visitor visitor4;
 
     @Before
     public void setUp() {
@@ -28,4 +33,30 @@ public class RollercoasterTest {
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
     }
+
+    @Test
+    public void cantAccessIfTooShort() {
+        visitor1 = new Visitor(14, 100.00, 15.00);
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void cantAccessIfTooYoung() {
+        visitor2 = new Visitor(9, 200.00, 15.00);
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor2));
+    }
+
+    @Test
+    public void cantAccessIfTooShortAndYoung() {
+        visitor3 = new Visitor(1, 100.00, 15.00);
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor3));
+    }
+
+    @Test
+    public void canAccessIfOver145cmAnd12Years() {
+        visitor1 = new Visitor(14, 200.00, 15.00);
+        assertEquals(true, rollerCoaster.isAllowedTo(visitor4));
+    }
 }
+
+//Rollercoster implements ISecurity and requires a visitor to be over 145cm tall and over 12 years of age
